170cbc4f45fedf220e4b8cfee6a16dbcc129a235
kopano <kopano@penguinprotocols.com>
2021-12-16 11:31:12 +0200

    added functions to add rows and carts

--- html/js/personal.js
+++ html/js/personal.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
 
+	function addCart() {
+		$(".link_row").append(' \
+			<div class="col-md-3">\
+				<div class="card">\
+					<div class="card-body">\
+						<h5 class="card-title">BYU-Pathway Portal</h5>\
+						<a href="https://www.byupathway.org/sign-in" class="btn btn-secondary btn-lg btn-responsive" role="button" target="_blank">Log in page</a>\
+					</div>\
+				</div>\
+			</div>\
+		');
+	};
+
+	function addRow() {
+		$("div #links").append("this is added")
+	};
+
   	$("#create-link").click(function(){
   		$("#create_l_modal").show();
+
 		$("#create").click(function(){
-			alert("The create button is clicked");
-	  	});
+			addRow();
+			// addCart();
+		});
 	});
 
 	$(".close").click(function(){
